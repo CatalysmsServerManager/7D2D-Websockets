a427c37a573045d496461583b62a662996d564f5
Niek Candaele <niekcandaele@gmail.com>
2021-01-12 18:12:48 +0100

    Better handling of startup timing

--- src/API.cs
+++ src/API.cs
@@ -8,26 +8,59 @@ namespace sdtd_apimod
     {
         public void InitMod()
         {
-            Log.Out("[CSMM WS] Runtime patching initialized");
-            Harmony harmony = new Harmony("com.github.CSMM");
-            Type[] logTypes = { typeof(string) };
-            MethodInfo original = AccessTools.Method(typeof(Log), "Out", logTypes);
-            if (original == null)
-            {
-                Log.Out("[CSMM WS] Injection failed: Log#Out method was not found");
-            }
-            else
-            {
-                MethodInfo postfix = typeof(Injections).GetMethod("Log_Postfix");
-                if (postfix == null)
-                {
-                    Log.Out("[CSMM WS] Injection failed: Log#Out postfix not found");
-                    return;
-                }
-                harmony.Patch(original, null, new HarmonyMethod(postfix));
-                Log.Out("[CSMM WS] Injected Log#Out");
+            ModEvents.GameStartDone.RegisterHandler(GameStartDone);
+            ModEvents.GameShutdown.RegisterHandler(GameShutdown);
+        }
 
+        private void GameStartDone()
+        {
+            try
+            {
+                CSMMLog.Out("Runtime patching initialized");
+                Harmony harmony = new Harmony("com.github.CSMM");
+                Type[] logTypes = { typeof(string) };
+                MethodInfo original = AccessTools.Method(typeof(Log), "Out", logTypes);
+                if (original == null)
+                {
+                    CSMMLog.Out("Injection failed: Log#Out method was not found");
+                }
+                else
+                {
+                    MethodInfo postfix = typeof(Injections).GetMethod("Log_Postfix");
+                    if (postfix == null)
+                    {
+                        CSMMLog.Out("Injection failed: Log#Out postfix not found");
+                        return;
+                    }
+                    harmony.Patch(original, null, new HarmonyMethod(postfix));
+                    CSMMLog.Out("Injected Log#Out");
+
+                }
             }
+            catch (ReflectionTypeLoadException ex)
+            {
+                // now look at ex.LoaderExceptions - this is an Exception[], so:
+                foreach (Exception inner in ex.LoaderExceptions)
+                {
+                    // write details of "inner", in particular inner.Message
+                    CSMMLog.Out(inner.Message);
+                }
+            }
+
+        }
+
+        private void GameShutdown()
+        {
+            // Shut down the websocket server
+        }
+
+    }
+
+    public class CSMMLog
+    {
+        public static void Out(String msg)
+        {
+            Log.Out("[CSMM WS] " + msg);
         }
     }
 }
